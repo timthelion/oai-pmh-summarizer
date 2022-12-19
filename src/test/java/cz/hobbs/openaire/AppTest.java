6a53f9ae3aaf29c0cff2d89ce679f05004a29e64
Timothy Hobbs <timothy@hobbs.cz>
2022-12-19 10:31:33 +0100

    Test argument parsing

--- src/test/java/cz/hobbs/openaire/AppTest.java
+++ src/test/java/cz/hobbs/openaire/AppTest.java
@@ -1,20 +1,24 @@
 package cz.hobbs.openaire;
+import cz.hobbs.openaire.App;
+import java.util.Optional;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void parseArgsGetsURI()
     {
-        assertTrue( true );
+        try {
+            assertEquals(App.parse_args(new String[] {"-e", "foo"}), Optional.of("foo"));
+        } catch (Exception e){
+            assertTrue(false);
+        }
     }
 }
